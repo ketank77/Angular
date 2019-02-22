8ff3cb4d93496fa47c27e6b7c48ef21baff519d1
Ketan <ketankumar000@gmail.com>
2019-02-22 05:59:35 +0530

    Header footer angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,16 @@ import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
