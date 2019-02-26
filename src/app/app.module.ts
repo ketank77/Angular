d68b944d386639e6503ff6c18e6af196f66fb5e1
Ketan <ketankumar000@gmail.com>
2019-02-26 08:23:12 +0530

    Form validaitons update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -66,7 +67,8 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   entryComponents: [LoginComponent],
 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from "../shared/dish";
 import { DishService } from '../services/dish.service';
-
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { Comment } from '../shared/comment';
 
 
 @Component({
@@ -19,9 +20,38 @@ export class DishdetailComponent implements OnInit {
     prev: string;
     next: string;
     dish : Dish;
+    commentForm: FormGroup;
+    comment: Comment;
+    showcomment: Comment;
+
+    formErrors = {
+      'author': '',
+      'rating': '',
+      'comment': ''
+    };
+    validationMessages = {
+      'author': {
+        'required':
+          'Name is required.',
+        'minlength':
+          'Name must be at least 2 characters long.'
+      },
+      'rating': {
+        'nullValidator':
+          'Rating should not be null.'
+      },
+      'comment': {
+        'required':
+          'Comment is required.'
+      },
+    };
+
     constructor(private dishservice: DishService,
       private route: ActivatedRoute,
-      private location: Location) { }
+      private location: Location,
+      private fb: FormBuilder) { 
+        this.createForm();
+      }
 
       ngOnInit() {
         this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -37,4 +67,64 @@ export class DishdetailComponent implements OnInit {
    goBack(): void {
       this.location.back();
    }
+
+   createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      rating: [5, Validators.nullValidator],
+      comment: ['', Validators.required]
+    });
+    this.commentForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set validation messages now
+   }
+
+   onValueChanged(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+    // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+    this.getCommentPreview();
+    }
+
+    getCommentPreview() {
+      if (!this.commentForm.valid) {
+        return;
+      }
+      this.commentForm = this.commentForm.value;
+    }
+
+    onSubmit() {
+      this.comment = this.commentForm.value;
+      console.log(this.comment);
+      const date = new Date();
+      this.comment.date = date.toString();
+      this.dish.comments.push(this.comment);
+      this.resetForm(this.commentForm);
+    }
+
+    resetForm (formGroup: FormGroup) {
+      let control: AbstractControl = null;
+      formGroup.reset({author: '', rating : 5, comment : ''});
+      formGroup.markAsUntouched();
+      Object.keys(formGroup.controls).forEach((name) => {
+        control = formGroup.controls[name];
+        control.setErrors(null);
+      });
+    }
 }
