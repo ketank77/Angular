3498b68389eb15eb8c03ed8236e0be12b6e73c06
Ketan <ketankumar000@gmail.com>
2019-03-01 21:09:49 +0530

    Angular Animations-2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 import { DishService } from '../services/dish.service';
+import { flyInOut,expand } from '../animations/app-animation';
 
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+  animations:[
+    flyInOut(),
+    expand()
+  ]
 })
 
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
   errMess : string;
 
    constructor( private dishService: DishService,
